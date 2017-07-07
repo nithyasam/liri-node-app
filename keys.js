221aa075a8c146e59e50872597fbc4c6683a1dcd
Nithya Sam <nithyasam@nithyas-air.gateway.pace.com>
2017-07-07 15:38:13 -0500

    keys.js line reindented

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '74wPB7JOTeiV8P9vv8UcdqstU',
-  consumer_secret: 'SbmvpebnuIEdgjVG4nMnwZAdVCpGZMEBBeSipXdWoUTcYM6lPC',
-  access_token_key: '880202482043027456-80BbMwgNr9C90BIxe8lGgbPxRLPIjNG',
-  access_token_secret: 'LpTNU4I23vz8kjib9ypqWC47dVyQmjb5HrHXeCCa9aOA6',
+	consumer_key: '74wPB7JOTeiV8P9vv8UcdqstU',
+	consumer_secret: 'SbmvpebnuIEdgjVG4nMnwZAdVCpGZMEBBeSipXdWoUTcYM6lPC',
+	access_token_key: '880202482043027456-80BbMwgNr9C90BIxe8lGgbPxRLPIjNG',
+	access_token_secret: 'LpTNU4I23vz8kjib9ypqWC47dVyQmjb5HrHXeCCa9aOA6',
 }
 
 exports.spotifyKeys = {
